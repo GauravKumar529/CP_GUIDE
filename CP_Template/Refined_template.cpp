b0540361b44e8150af46efbb6c6bf1c14dcdc6a4
Gaurav Kumar <rajputgaurav9582@gmail.com>
2025-08-24 23:51:33 +0530

    Create Refined_template.cpp

---
+++ CP_Template/Refined_template.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+#define int long long           // use long long everywhere
+#define pb push_back            // vector push_back
+#define f first
+#define s second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define rep(i,n) for (int i = 0; i < (n); i++)
+#define rep1(i,n) for (int i = 1; i <= (n); i++)
+#define repr(i,n) for (int i = (n)-1; i >= 0; i--)
+#define forn(i,a,b) for (int i = (a); i < (b); i++)
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl "\n" 
+#define add(a,b) ((a%MOD + b%MOD) % MOD)
+#define sub(a,b) ((a%MOD - b%MOD + MOD) % MOD)
+#define mul(a,b) ((a%MOD * b%MOD) % MOD)
+#define lb lower_bound
+#define ub upper_bound
+#define out(x) cout << x << "\n"
+#define out2(x,y) cout << x << " " << y << "\n"
+#define in(n) int n; cin >> n
+#define in2(a,b) int a,b; cin>>a>>b
+#define in3(a,b,c) int a,b,c; cin>>a>>b>>c
+#define inArr(a,n) vector<int> a(n); rep(i,n) cin>>a[i]
+
+#define debug(x) cerr << #x << " = " << x << "\n"
+
+// Constants
+// const int INF = 1e18;
+// const int MOD = 1e9+7;
+
+void solve() {
+    /// solve your  logic here
+    
+}
+
+int  main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    // cin >> t;   // uncomment for multiple test cases
+    while(t--) solve();
+    return 0 ;
+}
